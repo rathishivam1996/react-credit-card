8f3096f5ec944ed6978da9620dfcf2383dfd13fe
Shivam Rathi <shivamrathi2.sr@gmail.com>
2023-10-13 20:54:17 +0530

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,32 +4,14 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
-  parser: '@babel/eslint-parser',
+  overrides: [],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier', '@babel'],
+  plugins: ['react', 'prettier'],
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': ['error', { singleQuote: true, endOfLine: 'auto' }],
   },
 };
